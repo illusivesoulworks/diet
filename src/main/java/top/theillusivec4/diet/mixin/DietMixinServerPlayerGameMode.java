ff4a6b8dd96a5a5d81264fb751288cedb1aa10fa
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2022-01-31 02:28:16 -0800

    fix error in getting stacks from state, closes #110

--- src/main/java/top/theillusivec4/diet/mixin/DietMixinServerPlayerGameMode.java
+++ src/main/java/top/theillusivec4/diet/mixin/DietMixinServerPlayerGameMode.java
@@ -1,16 +1,19 @@
 package top.theillusivec4.diet.mixin;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerPlayerGameMode;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import top.theillusivec4.diet.DietMod;
 import top.theillusivec4.diet.api.DietCapability;
 
 @SuppressWarnings("unused")
@@ -25,9 +28,18 @@ public class DietMixinServerPlayerGameMode {
   public void diet$preBlockActivated(ServerPlayer player, Level world, ItemStack stack,
                                      InteractionHand hand, BlockHitResult result,
                                      CallbackInfoReturnable<InteractionResult> cir) {
-    DietCapability.get(player).ifPresent(tracker -> tracker.captureStack(
-        player.level.getBlockState(result.getBlockPos())
-            .getCloneItemStack(result, world, result.getBlockPos(), player)));
+    DietCapability.get(player).ifPresent(tracker -> {
+      BlockPos pos = result.getBlockPos();
+      BlockState state = player.level.getBlockState(pos);
+      ItemStack blockStack;
+      try {
+        blockStack = state.getCloneItemStack(result, world, pos, player);
+      } catch (Exception e) {
+        DietMod.LOGGER.debug("Error getting stack from {}", state);
+        return;
+      }
+      tracker.captureStack(blockStack);
+    });
   }
 
   @Inject(
