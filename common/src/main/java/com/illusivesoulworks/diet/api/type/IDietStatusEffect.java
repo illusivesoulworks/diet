acdbf1d56031d1e037cbe1a800a58e82b3987143
C4 <29991504+TheIllusiveC4@users.noreply.github.com>
2023-06-26 01:00:07 -0700

    Update copyright

--- common/src/main/java/com/illusivesoulworks/diet/api/type/IDietStatusEffect.java
+++ common/src/main/java/com/illusivesoulworks/diet/api/type/IDietStatusEffect.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2021-2023 Illusive Soulworks
+ *
+ * Diet is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Diet is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Diet.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.illusivesoulworks.diet.api.type;
 
 import net.minecraft.world.effect.MobEffect;
